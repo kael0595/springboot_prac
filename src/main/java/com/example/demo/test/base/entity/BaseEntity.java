c9d922ef6617f25893487b68060c6444e3a04367
kael0595 <changgyu549@gmail.com>
2024-07-19 09:45:45 +0900

    BaseEntity 수정

--- src/main/java/com/example/demo/test/base/entity/BaseEntity.java
+++ src/main/java/com/example/demo/test/base/entity/BaseEntity.java
@@ -1,14 +1,19 @@
 package com.example.demo.test.base.entity;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     private LocalDate createDate;
